77d7b89fd674fad7f9d2d6754394d6a3ee3dddb3
TNM88 <shahoriar2001@gmail.com>
2024-12-03 18:47:01 +0600

    10

---
+++ Class Work/10string.py
@@ -0,0 +1,25 @@
+a = "hello"
+b = "b2b2b2"
+c = "3g3g"
+
+d = a + b + c
+print(d)
+
+print(len(d))
+print(d[:-3])
+
+print("a2" in d)
+
+print(d.upper())
+print(d.lower())
+print(d.title())
+print(d.strip())
+print(d.isdigit())
+print(d.find("3g"))
+print(d.capitalize())
+print(d.isalnum())
+print(d.count("b2"))
+print(d.split())
+print(d.swapcase())
+print(d.lstrip("h"))
+print(d.replace("hello", "python"))
